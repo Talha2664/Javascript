dd87df27679f915f1ff23199d28447be1e52df09
Talha2664 <talhamughal02148@gmail.com>
2025-11-12 19:36:20 +0500

    add files

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+// ***************** Primitive datatypes ****************
+
+let name = "Talha";      // String
+let age = 21;            // Number
+let isStudent = true;    // Boolean
+let salary;              // Undefined
+let score = null;        // Null
+let id = Symbol("id");   // Symbol
+let bigNumber = 9999999999999999n; // BigInt
+
+// Non - Primitive datatypes
+
+let person = {name: "Talha", age: 21 };   // Object
+let numbers = [10, 20, 30, 40];            // Array
+function greet() {                         // Function
+  console.log("Hello Talha!");
+}
+
+
---
+++ object.js
@@ -0,0 +1,26 @@
+// Creating an Object
+
+let student = {
+    name : "Talha",
+    age : 23,
+    cgpa : 8.0,
+    show : function() {
+        console.log(`${this.name} age is ${this.age} `);  //the keyword #this use to refer to the object’s own properties.
+    }
+};
+// print object properties
+student.show()  
+console.log(student.cgpa)
+
+// Adding, Changing, and Deleting Properties
+
+let user = { name: "Talha" };
+
+// Add new property
+user.age = 21;
+
+// Change value
+user.name = "Talha Mughal";
+
+// Delete property
+delete user.age;
