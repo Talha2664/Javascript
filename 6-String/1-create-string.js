2d56fc1b37e3401ee09aa585fa9ea9c47b63aae6
Talha2664 <talhamughal02148@gmail.com>
2025-11-14 17:37:00 +0500

    add files

---
+++ 6-String/1-create-string.js
@@ -0,0 +1,9 @@
+// String
+
+let name1 = "Talha";      // Double quotes
+let name2 = 'Mughal';     // Single quotes
+let name3 = `Hello ${name1}`; // Backticks (Template Literal)
+
+console.log(name1); // Talha
+console.log(name2); // Mughal
+console.log(name3); // Hello Talha
--- 6-String/2-String-methods.js
+++ 6-String/2-String-methods.js
@@ -1,12 +1,3 @@
-// String
-
-let name1 = "Talha";      // Double quotes
-let name2 = 'Mughal';     // Single quotes
-let name3 = `Hello ${name1}`; // Backticks (Template Literal)
-
-console.log(name1); // Talha
-console.log(name2); // Mughal
-console.log(name3); // Hello Talha
 
 // ****************** String Methods ****************
 
