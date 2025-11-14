45fb968d50fdf064d76d1f6f8453181e8f5897f9
Talha2664 <talhamughal02148@gmail.com>
2025-11-14 13:06:08 +0500

    add files

--- practicefunction4.js
+++ practicefunction4.js
@@ -1,11 +1,20 @@
 function vowel(str){
     let arr = str.split("");
-    for(let i = 0; i<arr.length; i++){
-        if (arr[i] == a || arr[i] == e || arr[i] == i || arr[i] == o || arr[i] == u ){
-            console.log(arr[i])
+    let vowelsFound = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if (
+            arr[i] == "a" || 
+            arr[i] == "e" || 
+            arr[i] == "i" || 
+            arr[i] == "o" || 
+            arr[i] == "u"
+        ){
+            vowelsFound.push(arr[i]); 
         }
     }
-    return str
+    return vowelsFound;
 }
-let res = vowel("hello")
-console.log(res)
+
+let res = vowel("hello");
+console.log(res); // ["e", "o"]
--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function vowel(str){
-    let arr = str.split("");
-    console.log(arr)
-    for(let i = 0; i<arr.length; i++){
-        if (arr[i] == a || arr[i] == e || arr[i] == i || arr[i] == o || arr[i] == u ){
-            console.log(arr[i])
-        }
-    }
