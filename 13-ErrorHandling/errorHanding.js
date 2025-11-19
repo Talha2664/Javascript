dce8ed8edd3dceaf97f5fdc13de3cb7e62d59777
Talha2664 <talhamughal02148@gmail.com>
2025-11-19 22:50:11 +0500

    add new file

---
+++ 13-ErrorHandling/errorHanding.js
@@ -0,0 +1,17 @@
+// try...catch is used to handle errors in JavaScript.
+
+// try → contains code that may throw errors.
+try{
+    console.log(x);
+}
+// catch → handles the errors.
+catch(err){
+    console.log(err.message);
+    
+}
+// finally → runs always, even if no error occurs.
+finally{
+    console.log("Finally always runs!");
+    
+}
+
---
+++ 13-ErrorHandling/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let a;
+
+if (a===0){
+    throw ("Error Occured")
+}
+console.log(10/0);
